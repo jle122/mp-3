ea8c5268b119e13efb7a2639335f745bc09b7e7a
James Le <jle4231@gmail.com>
2025-07-21 23:00:22 -0400

    fix with photo not showing after deployment

--- src/components/mains/Home.tsx
+++ src/components/mains/Home.tsx
@@ -2,7 +2,7 @@ export default function Home() {
     return(
         <>
             <h3>Home</h3>
-            <img src="src\components\mains\6B79D2FE-49BA-4DB6-A07A-01E86681B31A.png"/>
+            <img src="/6B79D2FE-49BA-4DB6-A07A-01E86681B31A.png"/>
             <p>
                 Hello! My name is James Le and I am an undergraduate computer science major at BU.
                 Welcome to my website!
